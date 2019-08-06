6f632e3f152592a3afa983c3c59e8285493eeb7a
karla <karla.pantelimon@cst-dev.com>
2019-08-06 14:11:56 +0300

    Bug fix

--- cnfis-raw-data-parsing-script.py
+++ cnfis-raw-data-parsing-script.py
@@ -7,39 +7,47 @@ with open('cnfis-raw-data.txt', 'r') as file:
 new_list = re.compile(" U[0-9][0-9]").split(data)
 new_list[0] = new_list[0][3:]
 
-# for i in range(0, len(new_list)):
-#     if i > 15:
-#         j = i + 2
-#     else:
-#         j = i + 1
-#     print(j, new_list[i])
-
 testUni = new_list[0]
 
 oneUniList = testUni.split(" ")
 print(oneUniList)
 finalDictionary = []
+index = 0;
 for oneUniList in new_list:
-    oneUniList = oneUniList.split(" ");
-    name = '';
-    model_uni = {}
-    model_uni['total_students'] = float(oneUniList[len(oneUniList) - 12])
-    model_uni['buget_students'] = float(oneUniList[len(oneUniList) - 11])
-    model_uni['tax_students'] = float(oneUniList[len(oneUniList) - 10])
-    model_uni['total_licenta_students'] = float(oneUniList[len(oneUniList) - 9])
-    model_uni['buget_licenta_students'] = float(oneUniList[len(oneUniList) - 8])
-    model_uni['tax_licenta_students'] = float(oneUniList[len(oneUniList) - 7])
-    model_uni['total_masterat_students'] = float(oneUniList[len(oneUniList) - 6])
-    model_uni['buget_masterat_students'] = float(oneUniList[len(oneUniList) - 5])
-    model_uni['tax_masterat_students'] = float(oneUniList[len(oneUniList) - 4])
-    model_uni['total_doctorat_students'] = float(oneUniList[len(oneUniList) - 3])
-    model_uni['buget_doctorat_students'] = float(oneUniList[len(oneUniList) - 2])
-    model_uni['tax_doctorat_students'] = float(oneUniList[len(oneUniList) - 1])
+    oneUniList = oneUniList.split(" ")
+    name = ''
+    id = 'U'
+    j = ''
+    if index > 15:
+        j = index + 2
+    else:
+        j = index + 1
+    if j > 9:
+        id = 'U' + str(j)
+    else:
+        id = 'U0' + str(j)
+    model_uni = {
+        'id':id,
+        'id_num': j,
+        'total_students': int(oneUniList[len(oneUniList) - 12].replace('.', '')),
+        'buget_students': int(oneUniList[len(oneUniList) - 11].replace('.', '')),
+        'tax_students': int(oneUniList[len(oneUniList) - 10].replace('.', '')),
+        'total_licenta_students': int(oneUniList[len(oneUniList) - 9].replace('.', '')),
+        'buget_licenta_students': int(oneUniList[len(oneUniList) - 8].replace('.', '')),
+        'tax_licenta_students': int(oneUniList[len(oneUniList) - 7].replace('.', '')),
+        'total_masterat_students': int(oneUniList[len(oneUniList) - 6].replace('.', '')),
+        'buget_masterat_students': int(oneUniList[len(oneUniList) - 5].replace('.', '')),
+        'tax_masterat_students': int(oneUniList[len(oneUniList) - 4].replace('.', '')),
+        'total_doctorat_students': int(oneUniList[len(oneUniList) - 3].replace('.', '')),
+        'buget_doctorat_students': int(oneUniList[len(oneUniList) - 2].replace('.', '')),
+        'tax_doctorat_students': int(oneUniList[len(oneUniList) - 1].replace('.', ''))
+    }
     i = 13
     while i < len(oneUniList):
         name = oneUniList[len(oneUniList) - i] + ' ' + name;
         i = i + 1
     model_uni['name'] = name
     finalDictionary.append(model_uni);
+    index = index + 1;
 
 print(finalDictionary);
